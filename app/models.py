9fd32569df6ea8107b714c755a5aee88592dd120
Eleanor Kelman <ekelman3@gmail.com>
2023-06-23 16:05:53 -0400

    added sqlalchemy models

--- app/models.py
+++ app/models.py
@@ -4,6 +4,43 @@
 
 # Import from sqlalchemy.ext.declarative, declarative_base  
 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class Pet(db.Model):
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    breed = db.Column(db.String)
+    temperament = db.Column(db.String)
+
+    owners_of_pet = db.relationship('PersonPet', backref = 'pet')
+
+class Person(db.Model):
+
+    __tablename__ = "people"
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+
+    pets_of_owner = db.relationship('PersonPet', backref = 'person')
+
+class PersonPet(db.Model):
+
+    __tablename__ = "people_pets"
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    pet_id = db.Column(db.Integer, db.ForeignKey('pets.id'))
+    person_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+
 #1.a ✅ Initialize declarative_base and assign it to a variable called Base
 
 #1.b ✅ Create a class Pet that inherits from Base
